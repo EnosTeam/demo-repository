ad4bee7398ce433fe46d3e829c16f59a1fb20e6c
Song-min-jae <imsong1024@naver.com>
2024-09-30 13:54:56 +0900

    송민재_class Post 수정

--- FlaskApp/KSC_Web_3.py
+++ FlaskApp/KSC_Web_3.py
@@ -13,6 +13,10 @@ class User(db.Model):
     birth_date = db.Column(db.String(10), nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    letter = db.Column(db.Text, nullable=False)
 with app.app_context():
     db.create_all()
 
