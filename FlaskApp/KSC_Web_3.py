f22dc2c79ffb219819f62700d59ecf02802c70b1
Song-min-jae <imsong1024@naver.com>
2024-10-03 13:03:16 +0900

    송민재_게시판 수정

--- FlaskApp/KSC_Web_3.py
+++ FlaskApp/KSC_Web_3.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
 app = Flask(__name__, static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
@@ -13,10 +14,12 @@ class User(db.Model):
     birth_date = db.Column(db.String(10), nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     letter = db.Column(db.Text, nullable=False)
+
 with app.app_context():
     db.create_all()
 
@@ -39,34 +42,30 @@ def inup():
 @app.route('/Signup', methods = ['Get', 'Post'])
 def Signup():
     if request.method == 'POST':
-        # Get form data from the request
         username = request.form.get('ID')
         password = request.form.get('PW')
         email = request.form.get('Mail')
         birth_date = request.form.get('BR')
         first_name = request.form.get('FN')
         last_name = request.form.get('LN')
-
-        # Create a new User object
         new_user = User(
             username=username,
-            password=password,  # You should hash the password for security purposes
+            password=password, 
             email=email,
             birth_date=birth_date,
             first_name=first_name,
             last_name=last_name
         )
-        
-        # Add the new user to the database
         db.session.add(new_user)
         db.session.commit()
-        
         return redirect(url_for('Signup'))
     return render_template('KSC_Web_Sign_up.html')
 
 @app.route('/noti')
 def noti():
     return render_template('KSC_Web_Notice.html')
+
+
 
 @app.route('/p')
 def index():
@@ -112,6 +111,83 @@ def search_post():
     else:
         posts = Post.query.all() 
     return render_template('KSC_Web_Post_Search.html', posts=posts, keyword=keyword)
+
+
+class Question_2(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    letter = db.Column(db.Text(), nullable = False)
+    
+with app.app_context():
+    db.create_all()    
+
+
+@app.route('/noti_QA')
+def noti_QA():
+    return render_template('KSC_Web_Notice_QA.html')
+
+
+@app.route('/noti_QA_q', methods=['POST'])
+def noti_QA_q():
+    title = request.form.get('title')
+    letter = request.form.get('letter')
+    new_Question =  Question_2(title=title, letter=letter)
+    db.session.add(new_Question)
+    db.session.commit()
+    return redirect(url_for('noti_QA'))
+
+
+
+
+
+# @app.route('/post/<int:post_id>')
+# def noti_QA_q_detail(post_id):
+#     Question = Question_2.query.get_or_404(Question_2_id)
+#     return render_template('post_detail.html', Question=Question)
+
+
+
+
+
+
+
+
+# @app.route('/post/delete/<int:post_id>', methods=['POST'])
+# def delete_post(post_id):
+#     post = Post.query.get_or_404(post_id)
+#     db.session.delete(post)
+#     db.session.commit()
+#     return redirect(url_for('index'))
+
+# @app.route('/post/edit/<int:post_id>', methods=['GET', 'POST'])
+# def edit_post(post_id):
+#     post = Post.query.get_or_404(post_id)
+#     if request.method == 'POST':
+#         post.title = request.form['title']
+#         post.letter = request.form['letter']
+#         db.session.commit()
+#         return redirect(url_for('post_detail', post_id=post.id))
+#     return render_template('edit_post.html', post=post)
+
+# @app.route('/search', methods=['GET'])
+# def search_post():
+#     keyword = request.args.get('keyword', '')
+#     if keyword:
+#         posts = Post.query.filter(or_(Post.title.contains(keyword), Post.letter.contains(keyword))).all()
+#     else:
+#         posts = Post.query.all() 
+#     return render_template('KSC_Web_Post_Search.html', posts=posts, keyword=keyword)
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
