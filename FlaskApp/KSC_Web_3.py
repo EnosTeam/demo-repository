dd35511512bd42ed49bc5ea050de5cf1aa9eead3
Mirim <marinrim23@gmail.com>
2024-10-05 10:42:58 +0900

    Thursday final

--- FlaskApp/KSC_Web_3.py
+++ FlaskApp/KSC_Web_3.py
@@ -1,16 +1,13 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, redirect, url_for,flash, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
-
 app = Flask(__name__, static_folder='static')
 app.secret_key = 'supersecretkey12345'  # 경설컴 예시 secret key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
 
-
 db = SQLAlchemy(app)
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
@@ -20,11 +17,22 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
 
+class Question_2(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    letter = db.Column(db.Text(), nullable = False)
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    letter = db.Column(db.Text, nullable=False)
+    question = db.relationship('Question_2', backref=db.backref('answers', lazy=True))
+    question_id = db.Column(db.Integer, db.ForeignKey('question_2.id'), nullable=False)
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     letter = db.Column(db.Text, nullable=False)
-
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def door():
@@ -92,6 +100,66 @@ def Signup():
         return redirect(url_for('welcome', username=first_name))
     return render_template('KSC_Web_Sign_up.html')
 
+@app.route('/noti_QA')
+def noti_QA():
+    questions = Question_2.query.all()
+    return render_template('KSC_Web_Notice_QA.html', questions=questions)
+
+
+@app.route('/noti_QA_q', methods=['POST'])
+def noti_QA_q():
+    title = request.form.get('title')
+    letter = request.form.get('letter')
+    new_Question =  Question_2(title=title, letter=letter)
+    db.session.add(new_Question)
+    db.session.commit()
+    return redirect(url_for('noti_QA'))
+
+@app.route('/noti_QA/<int:question_id>/detail')
+def noti_QA_q_detail(question_id):
+    question = Question_2.query.get_or_404(question_id)
+    answers = Answer.query.filter_by(question_id=question_id).all()
+    return render_template('notice_detail.html', question=question, answers=answers)
+
+
+@app.route('/noti_QA/search', methods=['GET'])
+def search_question():
+     keyword = request.args.get('keyword', '')
+     if keyword:
+         questions = Question_2.query.filter(or_(Question_2.title.contains(keyword), Question_2.letter.contains(keyword))).all()
+     else:
+         questions = Question_2.query.all() 
+     return render_template('KSC_Web_Notice_QA_Search.html', posts=questions, keyword=keyword)
+
+@app.route('/noti_QA/edit/<int:question_id>', methods=['GET', 'POST'])
+def edit_question(question_id):
+    question = Question_2.query.get_or_404(question_id)
+    if request.method == 'POST':
+        question.title = request.form['title']
+        question.letter = request.form['letter']
+        db.session.commit()
+        return redirect(url_for('noti_QA_q_detail', question_id=question.id))
+    
+    return render_template('edit_question.html', question=question)
+
+@app.route('/noti_QA/delete/<int:question_id>', methods=['POST'])
+def delete_question(question_id):
+    question = Question_2.query.get_or_404(question_id)
+    db.session.delete(question)
+    db.session.commit()
+    return redirect(url_for('noti_QA'))
+
+
+    
+@app.route('/noti_QA/<int:question_id>/answer', methods=['GET', 'POST'])
+def submit_answer(question_id):
+    title = request.form.get('title')
+    letter = request.form.get('letter')
+    new_answer = Answer(title=title, letter=letter, question_id=question_id)
+    db.session.add(new_answer)
+    db.session.commit()
+    return redirect(url_for('noti_QA_q_detail', question_id=question_id))
+
 @app.route('/mypage')
 def mypage():
     # Ensure user is logged in
@@ -123,7 +191,7 @@ def delete_account():
     else:
         flash('You are not logged in.', 'danger')
         return redirect(url_for('inup'))    
-
+    
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)  # Clear the login session
@@ -136,7 +204,8 @@ def noti():
 
 @app.route('/gallery')
 def gallery():
-    return render_template('gallery.html')    
+    return render_template('gallery.html')
+
 
 @app.route('/p')
 def index():
@@ -187,81 +256,5 @@ def search_post():
 def welcome(username):
     return render_template('welcome.html', username=username)
 
-class Question_2(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(200), nullable = False)
-    letter = db.Column(db.Text(), nullable = False)
-
-
-@app.route('/noti_QA')
-def noti_QA():
-    questions = Question_2.query.all()
-    return render_template('KSC_Web_Notice_QA.html', questions=questions)
-
-
-@app.route('/noti_QA_q', methods=['POST'])
-def noti_QA_q():
-    title = request.form.get('title')
-    letter = request.form.get('letter')
-    new_Question =  Question_2(title=title, letter=letter)
-    db.session.add(new_Question)
-    db.session.commit()
-    return redirect(url_for('noti_QA'))
-
-@app.route('/noti_QA/<int:question_id>/detail')
-def noti_QA_q_detail(question_id):
-    question = Question_2.query.get_or_404(question_id)
-    answers = Answer.query.filter_by(question_id=question_id).all()
-    return render_template('notice_detail.html', question=question, answers=answers)
-
-
-@app.route('/noti_QA/search', methods=['GET'])
-def search_question():
-     keyword = request.args.get('keyword', '')
-     if keyword:
-         questions = Question_2.query.filter(or_(Question_2.title.contains(keyword), Question_2.letter.contains(keyword))).all()
-     else:
-         questions = Question_2.query.all() 
-     return render_template('KSC_Web_Notice_QA_Search.html', posts=posts, keyword=keyword)
-
-@app.route('/noti_QA/edit/<int:question_id>', methods=['GET', 'POST'])
-def edit_question(question_id):
-    question = Question_2.query.get_or_404(question_id)
-    if request.method == 'POST':
-        question.title = request.form['title']
-        question.letter = request.form['letter']
-        db.session.commit()
-        return redirect(url_for('noti_QA_q_detail', question_id=question.id))
-    
-    return render_template('edit_question.html', question=question)
-
-@app.route('/noti_QA/delete/<int:question_id>', methods=['POST'])
-def delete_question(question_id):
-    question = Question_2.query.get_or_404(question_id)
-    db.session.delete(question)
-    db.session.commit()
-    return redirect(url_for('noti_QA'))
-
-
-class Answer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    letter = db.Column(db.Text, nullable=False)
-    question = db.relationship('Question_2', backref=db.backref('answers', lazy=True))
-    question_id = db.Column(db.Integer, db.ForeignKey('question_2.id'), nullable=False)
-    
-@app.route('/noti_QA/<int:question_id>/answer', methods=['GET', 'POST'])
-def submit_answer(question_id):
-    title = request.form.get('title')
-    letter = request.form.get('letter')
-    new_answer = Answer(title=title, letter=letter, question_id=question_id)
-    db.session.add(new_answer)
-    db.session.commit()
-    return redirect(url_for('noti_QA_q_detail', question_id=question_id))
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-with app.app_context():
-    db.create_all()
