5d882cdff59c56892af09252480d18057937bdf0
Song-min-jae <imsong1024@naver.com>
2024-11-02 21:29:52 +0900

    Add files via upload

---
+++ FlaskApp_3/KSC_Web_Introduce.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, render_template, redirect, url_for,flash, session, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_
+
+Introduce = Blueprint('Introduce', __name__)
+
+@Introduce.route('/i')
+def intro():
+    return render_template('KSC_Web_introduce.html')
