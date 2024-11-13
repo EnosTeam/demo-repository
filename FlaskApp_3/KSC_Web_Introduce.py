43a9a7aff4f0c8f57d46d4a0e25600493676f738
Song-min-jae <imsong1024@naver.com>
2024-11-13 16:58:40 +0900

    Add files via upload

--- FlaskApp_3/KSC_Web_Introduce.py
+++ FlaskApp_3/KSC_Web_Introduce.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for,flash, session, Blueprint
+from flask import Flask, request, render_template, redirect, url_for, flash, session, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
 
--- FlaskApp_3/KSC_Web_Main_New.py
+++ FlaskApp_3/KSC_Web_Main_New.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for,flash, session, Blueprint
+from flask import Flask, request, render_template, redirect, url_for, flash, session, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
 app = Flask(__name__, static_folder='static')
